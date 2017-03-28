4cf3c2a5b25ba1c86f31f80fbfbf7dcfe8210541
Evelyn Hernández <dark.desires@live.cl>
2017-03-28 11:29:52 -0300

    Cambio para dmz de dirección REST

--- src/app/services/factigisVE/layers-service.js
+++ src/app/services/factigisVE/layers-service.js
@@ -14,9 +14,9 @@ function getMapLayers() {
 }
 
 function myLayers(){
-  const serviceMain = 'http://gisred.chilquinta/arcgis/';
+  //const serviceMain = 'http://gisred.chilquinta/arcgis/';
   //change this for external connection:
-  //const serviceMain = 'http://gisred.chilquinta.cl:5555/arcgis/';
+  const serviceMain = 'http://gisred.chilquinta.cl:5555/arcgis/';
   const serviceURL = serviceMain + 'rest/services/';
   //var graphicLayer = new GraphicsLayer;
 
