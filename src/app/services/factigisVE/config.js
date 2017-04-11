359a2924cb349450c9cf02054d50a4aa3543815d
Evelyn Hernández <dark.desires@live.cl>
2017-04-11 11:01:55 -0300

    Req #3 prod. Tb borrando elementos no usados en app

--- src/app/services/factigisVE/config.js
+++ src/app/services/factigisVE/config.js
@@ -1,4 +1,4 @@
-const env = {
+/*const env = {
   ROOT: "dist/css/",
   CSSDIRECTORY: 'dist/css/',
   ROUTEPATH: '/',
@@ -8,11 +8,13 @@ const env = {
   SAVEAPPLICATIONNAME: 'REACT_AP_WEB_DESA'
 }
 
-
-/*factigisVE  : v0.6.2
-Author: Evelyn Hernandez
 */
 /*
+Compilate: 11.04.2017
+factigisVE  : v0.6.2
+Author: Evelyn Hernandez
+*/
+
 const env = {
   ROOT: "css/",
   CSSDIRECTORY: 'css/',
@@ -22,6 +24,6 @@ const env = {
   SAVEAPPLICATIONMODULE: "FACTIGIS_PROD",
   SAVEAPPLICATIONNAME: 'REACT_FACTIGISVE_PROD'
 }
-*/
+
 
 export default env;
